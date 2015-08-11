331c00b775f3f043d2514a7652505b6f937b7f3a
Grishberg <grishberg@ya.ru>
2015-08-11 14:58:26 +0700

    initial commit

--- DbHelper.java
+++ DbHelper.java
@@ -16,3 +16,41 @@ public class DbHelper extends SQLiteOpenHelper {
 	public static final String TABLE_TABLE_NAME_2 = "tableName2";
 	public static final String TABLE_NAME_2_FIELD_1 = "field1";
 	public static final String TABLE_NAME_2_FIELD_2 = "field2";
+
+	// tableName1
+	private static final String CREATE_TABLE_TABLE_NAME_1 ="" +
+		"CREATE TABLE "+TABLE_NAME_1 "("+
+		FIELD_1 + " primary key autoincrement ," +
+		FIELD_2 + " " +
+		");";
+	// tableName2
+	private static final String CREATE_TABLE_TABLE_NAME_2 ="" +
+		"CREATE TABLE "+TABLE_NAME_2 "("+
+		FIELD_1 + " ," +
+		FIELD_2 + " " +
+		");";
+	public DbHelper(Context context) {
+		super(context, DB_NAME, null, DB_VERSION);
+	}
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		String[] CREATES = {
+			CRATE_TABLE_TABLE_NAME_1,
+			CRATE_TABLE_TABLE_NAME_2
+		};
+		for (final String table : CREATES) {
+			db.execSQL(table);
+		}
+	};
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {	
+		String[] TABLES = {
+			TABLE_TABLE_NAME_1,
+			TABLE_TABLE_NAME_2
+		};
+		for (final String table : TABLES) {
+			db.execSQL("DROP TABLE IF EXISTS " + table);
+		}
+		onCreate(db);
+	}
+}
--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 __author__ = 'g'
 
+
 class Generator:
-    def __init__(self,rootPackage, dbName):
+    def __init__(self, rootPackage, dbName):
         self.pkg = rootPackage
         self.dbName = dbName
 
-    def getClassName(self,s):
+    def getClassName(self, s):
         out = s[0].upper() + s[1:]
         arr = out.split("_")
         if len(arr) > 0:
             out = ""
             for i in arr:
-                out += i[0].upper()+i[1:]
+                out += i[0].upper() + i[1:]
         return out
 
     def generateVarName(self, oldName):
@@ -34,7 +35,7 @@ class Generator:
         for i in name:
             if i.upper() == i:
                 if upper == 0:
-                    #camel
+                    # camel
                     out.append("_")
                     out.append(i)
                 else:
@@ -43,17 +44,17 @@ class Generator:
             else:
                 if upper == 1:
                     if out[-2] != '_':
-                        out.insert(-1,'_')
+                        out.insert(-1, '_')
                 upper = 0
                 out.append(i)
         sOut = ""
         for i in out:
             sOut += i
         if sOut.find("__") > 0:
-            sOut = sOut.replace("__","_")
+            sOut = sOut.replace("__", "_")
         return sOut.upper()
 
-    def parseTemp(self,inBuffer):
+    def parseTemp(self, inBuffer):
         lst = inBuffer.split("\n")
         tables = []
         tableName = None
@@ -63,54 +64,112 @@ class Generator:
             words = line.split(" ")
             if len(line) > 0 and len(words) == 1:
                 # table name
-                if tableName != None:
-                    tables.append([tableName,fields])
+                if tableName is not None:
+                    tables.append({"name": tableName, "fields": fields})
                 fields = []
                 tableName = line
             elif len(words) > 1:
-                #fields
+                # fields
                 fieldName = words[0]
                 fieldType = words[1]
+                fieldConstName =  self.generateConstName(fieldName)
                 fieldParam = ""
                 if len(words) > 2:
-                    for i in range(2,len(words)):
-                        fieldParam += words[i]
-                fields.append({'name': fieldName, 'type': fieldType, 'param': fieldParam})
-        if tableName != None:
-            tables.append([tableName,fields])
-
+                    for i in range(2, len(words)):
+                        fieldParam += words[i]+' '
+                fields.append({'name': fieldName, 'type': fieldType, 'param': fieldParam, 'const': fieldConstName})
+        if tableName is not None:
+            tables.append({"name": tableName, "fields": fields})
         self.generateHelper(tables)
 
-    def generateHelper(self,tables):
+    def generateHelper(self, tables):
         out = ""
-        out += "package "+self.pkg+";\n\n"
+        out += "package " + self.pkg + ";\n\n"
         out += "import android.content.ContentValues;\n"
         out += "import android.content.Context;\n"
         out += "import android.database.sqlite.SQLiteDatabase;\n"
         out += "import android.database.sqlite.SQLiteOpenHelper;\n"
         out += "public class DbHelper extends SQLiteOpenHelper {\n"
-        out += '\tprivate static final String DB_NAME = "'+self.dbName+'";\n'
+        out += '\tprivate static final String DB_NAME = "' + self.dbName + '";\n'
         out += "\tprivate static final int DB_VERSION = 1;\n"
         out += '\tpublic static final String COLUMN_ID = "_id";\n'
         for t in tables:
-            out += "\t// "+t[0]+"\n"
-            consTableName = self.generateConstName(t[0])
-            out += "\tpublic static final String TABLE_"+consTableName+' = "'+t[0]+'";\n'
-            for v in t[1]:
+            out += "\t// " + t["name"] + "\n"
+            constTableName = self.generateConstName(t["name"])
+            out += "\tpublic static final String TABLE_" + constTableName + ' = "' + t["name"] + '";\n'
+            for v in t["fields"]:
                 name = v["name"]
                 nameConst = self.generateConstName(name)
-                out += "\tpublic static final String "+consTableName +"_"+nameConst+' = "'+name+'";\n'
+                out += "\tpublic static final String " + constTableName + "_" + nameConst + ' = "' + name + '";\n'
 
+        out += "\n"
+        for t in tables:
+            fields = t["fields"]
+            tableName = t["name"]
+            out += "\t// " + tableName + "\n"
+            constTableName = self.generateConstName(tableName)
+            out += "\tprivate static final String CREATE_TABLE_"+ constTableName + ' ="" +\n'
+            out += '\t\t"CREATE TABLE "+'+constTableName + ' "("+\n'
+
+            for i in range(0, len(fields)):
+                field = fields[i]
+                out += '\t\t' + field['const'] + ' + " ' + field['param']
+                if i < len(fields) - 1:
+                    out += ","
+                out += '" +\n'
+
+            out += '\t\t");";\n'
+
+        out += "\tpublic DbHelper(Context context) {\n"
+        out += "\t\tsuper(context, DB_NAME, null, DB_VERSION);\n"
+        out += "\t}\n"
+
+        out += "\t@Override\n"
+        out += "\tpublic void onCreate(SQLiteDatabase db) {\n"
+        out += "\t\tString[] CREATES = {\n"
+        for i in range(len(tables)):
+            constTableName =  self.generateConstName( tables[i]["name"] )
+            out += "\t\t\t"+ "CRATE_TABLE_"+constTableName
+            if i < len(tables) - 1 :
+                out += ","
+            out += "\n"
+        out += "\t\t};\n"
+
+        out += "\t\tfor (final String table : CREATES) {\n"
+        out += "\t\t\t" + "db.execSQL(table);\n"
+        out += "\t\t}\n"
+        out += "\t};\n"
+
+        out += "\t@Override\n"
+        out += "\tpublic void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {\t\n"
+        out += "\t\tString[] TABLES = {\n"
+
+        for i in range(len(tables)):
+            constTableName = self.generateConstName( tables[i]["name"] )
+            out += "\t\t\t"+ "TABLE_"+constTableName
+            if i < len(tables) - 1 :
+                out += ","
+            out += "\n"
+
+        out += "\t\t};\n"
+
+        out += "\t\tfor (final String table : TABLES) {\n"
+        out += '\t\t\tdb.execSQL("DROP TABLE IF EXISTS " + table);\n'
+        out += '\t\t}\n'
+
+        out += '\t\tonCreate(db);\n'
+        out += "\t}\n"
         out += "}\n"
-        f = open("DbHelper.java","w")
+        f = open("DbHelper.java", "w")
         f.write(out)
         f.close()
+
 
 pkg = "com.test.data.db"
 dbName = "test.db"
 g = Generator(pkg, dbName)
 
-f = open("in.txt","r")
+f = open("in.txt", "r")
 buf = f.read()
 f.close()
 
